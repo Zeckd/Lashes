a221a8a0762c43be49e8b414aa9d4112247939fb
abaskan0v <iskenpubg@gmail.com>
2025-07-14 17:24:31 +0600

    update

--- src/main/java/kg/mega/lashes/controllers/HomeController.java
+++ src/main/java/kg/mega/lashes/controllers/HomeController.java
@@ -1,13 +1,12 @@
 package kg.mega.lashes.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+@Controller
 public class HomeController {
-
     @GetMapping("/")
     public String home() {
-        return "Приложение Lashes работает!";
+        return "index.html";  // отдаст static/index.html
     }
 }

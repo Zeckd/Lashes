a57e1783157d8c6805e8b519974e75d1c1f6d602
abaskan0v <iskenpubg@gmail.com>
2025-07-14 17:37:52 +0600

    update

--- src/main/java/kg/mega/lashes/controllers/HomeController.java
+++ src/main/java/kg/mega/lashes/controllers/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "index.html";  // отдаст static/index.html
+        return "redirect:/client.html";
     }
 }
